747da19f4b9ecbb3091300907bce6ca3ddf1abed
baochig <103627458+baochig@users.noreply.github.com>
2025-09-12 17:38:30 +0800

    CHTSystemAwake
    Update CHTSystemAwake for Win11

---
+++ CHTSystemAwake/CHTSystemAwake/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace CHTSystemAwake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.NotifyIcon = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(27, 28);
+            this.run.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(95, 28);
+            this.run.TabIndex = 0;
+            this.run.Text = "執行";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(188, 28);
+            this.stop.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(95, 28);
+            this.stop.TabIndex = 0;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.Location = new System.Drawing.Point(188, 80);
+            this.NotifyIcon.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.NotifyIcon.Name = "NotifyIcon";
+            this.NotifyIcon.Size = new System.Drawing.Size(95, 28);
+            this.NotifyIcon.TabIndex = 0;
+            this.NotifyIcon.Text = "背景執行";
+            this.NotifyIcon.UseVisualStyleBackColor = true;
+            this.NotifyIcon.Click += new System.EventHandler(this.NotifyIcon_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(317, 306);
+            this.Controls.Add(this.NotifyIcon);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.run);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button NotifyIcon;
+    }
+}
+
---
+++ CHTSystemAwake/CHTSystemAwake/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace CHTSystemAwake
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.NotifyIcon notifyIcon1; //建立icon的容器
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            ES_SYSTEM_REQUIRED = 0x00000001
+
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT = 0x00000004
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            //指定使用的容器
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            //建立NotifyIcon
+            this.notifyIcon1.Icon = new Icon(SystemIcons.Information, 40, 40);
+            this.notifyIcon1.Text = "System Awaker";
+            //點兩下Icon呼叫程式
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_Click);
+        }
+
+        private void run_Click(object sender, EventArgs e)
+        {
+            Form1.SetThreadExecutionState(Form1.EXECUTION_STATE.ES_CONTINUOUS |
+                                                          Form1.EXECUTION_STATE.ES_DISPLAY_REQUIRED |
+                                                          Form1.EXECUTION_STATE.ES_SYSTEM_REQUIRED |
+                                                          Form1.EXECUTION_STATE.ES_AWAYMODE_REQUIRED);
+        }
+
+        private void stop_Click(object sender, EventArgs e)
+        {
+            Form1.SetThreadExecutionState(Form1.EXECUTION_STATE.ES_CONTINUOUS);
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.notifyIcon1.Visible == false)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+            else
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.notifyIcon1.Visible = false;
+            }
+        }
+    }
+}
