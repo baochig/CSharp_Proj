ef620564a677612b933787472ad8dfa27704bd7c
baochig <103627458+baochig@users.noreply.github.com>
2025-11-06 10:28:31 +0800

    Add files via upload
    OCS_Database files

---
+++ CHT_OMP/OCS_Database/Db_Interface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCS_Database
+{
+    interface Db_Interface
+    {
+        bool Open(ref object connector);
+        bool Close(ref object connector);
+
+        string Verify();
+    }
+}
