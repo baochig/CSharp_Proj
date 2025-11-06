3a4aa909e577a41ca1e154c7dcea836d0767f0a2
baochig <103627458+baochig@users.noreply.github.com>
2025-11-06 10:29:08 +0800

    Add files via upload
    OCS_GUI files upload

---
+++ CHT_OMP/OCS_GUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using OCS_Parser_Minoss_GUI;
+
+namespace OCS_GUI
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+        }
+    }
+}
---
+++ CHT_OMP/OCS_GUI/menu.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OCS_GUI
+{
+    public partial class menu : Form
+    {
+        public menu()
+        {
+            InitializeComponent();
+        }
+    }
+}
