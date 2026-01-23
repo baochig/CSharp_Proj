d4ccd7e65d443e674cd0b1f68b7f9565914e985e
baochig <103627458+baochig@users.noreply.github.com>
2026-01-23 23:01:04 +0800

    Add files via upload

---
+++ WindowsMouseController/Form1.Designer.cs
@@ -0,0 +1,250 @@
+﻿namespace WindowsMouseController
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.run = new System.Windows.Forms.Button();
+            this.stop = new System.Windows.Forms.Button();
+            this.NotifyIcon = new System.Windows.Forms.Button();
+            this.click = new System.Windows.Forms.Button();
+            this.MixRun_1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ExtendHiGate = new System.Windows.Forms.Button();
+            this.textControlWindowName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonSendKey = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textKey = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textKeyInterval = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chBoxKeyPress = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // run
+            // 
+            this.run.Location = new System.Drawing.Point(34, 42);
+            this.run.Name = "run";
+            this.run.Size = new System.Drawing.Size(87, 23);
+            this.run.TabIndex = 0;
+            this.run.Text = "滑鼠移動";
+            this.run.UseVisualStyleBackColor = true;
+            this.run.Click += new System.EventHandler(this.run_Click);
+            // 
+            // stop
+            // 
+            this.stop.Location = new System.Drawing.Point(158, 42);
+            this.stop.Name = "stop";
+            this.stop.Size = new System.Drawing.Size(85, 23);
+            this.stop.TabIndex = 1;
+            this.stop.Text = "停止";
+            this.stop.UseVisualStyleBackColor = true;
+            this.stop.Click += new System.EventHandler(this.stop_Click);
+            // 
+            // NotifyIcon
+            // 
+            this.NotifyIcon.Location = new System.Drawing.Point(158, 102);
+            this.NotifyIcon.Name = "NotifyIcon";
+            this.NotifyIcon.Size = new System.Drawing.Size(85, 23);
+            this.NotifyIcon.TabIndex = 2;
+            this.NotifyIcon.Text = "背景執行";
+            this.NotifyIcon.UseVisualStyleBackColor = true;
+            this.NotifyIcon.Click += new System.EventHandler(this.NotifyIcon_Click);
+            // 
+            // click
+            // 
+            this.click.Location = new System.Drawing.Point(34, 72);
+            this.click.Name = "click";
+            this.click.Size = new System.Drawing.Size(87, 23);
+            this.click.TabIndex = 3;
+            this.click.Text = "座標自訂";
+            this.click.UseVisualStyleBackColor = true;
+            this.click.Click += new System.EventHandler(this.click_Click);
+            // 
+            // MixRun_1
+            // 
+            this.MixRun_1.Location = new System.Drawing.Point(34, 102);
+            this.MixRun_1.Name = "MixRun_1";
+            this.MixRun_1.Size = new System.Drawing.Size(87, 23);
+            this.MixRun_1.TabIndex = 4;
+            this.MixRun_1.Text = "左右連點";
+            this.MixRun_1.UseVisualStyleBackColor = true;
+            this.MixRun_1.Click += new System.EventHandler(this.MixRun_1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(34, 174);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(648, 285);
+            this.textBox1.TabIndex = 5;
+            // 
+            // ExtendHiGate
+            // 
+            this.ExtendHiGate.Location = new System.Drawing.Point(158, 71);
+            this.ExtendHiGate.Name = "ExtendHiGate";
+            this.ExtendHiGate.Size = new System.Drawing.Size(85, 23);
+            this.ExtendHiGate.TabIndex = 4;
+            this.ExtendHiGate.Text = "延長時間";
+            this.ExtendHiGate.UseVisualStyleBackColor = true;
+            this.ExtendHiGate.Click += new System.EventHandler(this.ExtendHiGate_Click);
+            // 
+            // textControlWindowName
+            // 
+            this.textControlWindowName.Location = new System.Drawing.Point(393, 40);
+            this.textControlWindowName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textControlWindowName.Name = "textControlWindowName";
+            this.textControlWindowName.Size = new System.Drawing.Size(104, 25);
+            this.textControlWindowName.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(295, 46);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 15);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Process ID: ";
+            // 
+            // buttonSendKey
+            // 
+            this.buttonSendKey.Location = new System.Drawing.Point(514, 37);
+            this.buttonSendKey.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSendKey.Name = "buttonSendKey";
+            this.buttonSendKey.Size = new System.Drawing.Size(85, 27);
+            this.buttonSendKey.TabIndex = 8;
+            this.buttonSendKey.Text = "重複按鍵";
+            this.buttonSendKey.UseVisualStyleBackColor = true;
+            this.buttonSendKey.Click += new System.EventHandler(this.buttonSendKey_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(295, 80);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 15);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Key Sending: ";
+            // 
+            // textKey
+            // 
+            this.textKey.Location = new System.Drawing.Point(393, 76);
+            this.textKey.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textKey.Name = "textKey";
+            this.textKey.Size = new System.Drawing.Size(104, 25);
+            this.textKey.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(271, 110);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(116, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "計時器週期(Sec):";
+            // 
+            // textKeyInterval
+            // 
+            this.textKeyInterval.Location = new System.Drawing.Point(393, 105);
+            this.textKeyInterval.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textKeyInterval.Name = "textKeyInterval";
+            this.textKeyInterval.Size = new System.Drawing.Size(104, 25);
+            this.textKeyInterval.TabIndex = 6;
+            this.textKeyInterval.Text = "5";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(271, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "按鍵按著:";
+            // 
+            // chBoxKeyPress
+            // 
+            this.chBoxKeyPress.AutoSize = true;
+            this.chBoxKeyPress.Location = new System.Drawing.Point(393, 141);
+            this.chBoxKeyPress.Name = "chBoxKeyPress";
+            this.chBoxKeyPress.Size = new System.Drawing.Size(58, 19);
+            this.chBoxKeyPress.TabIndex = 9;
+            this.chBoxKeyPress.Text = "Press";
+            this.chBoxKeyPress.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(702, 468);
+            this.Controls.Add(this.chBoxKeyPress);
+            this.Controls.Add(this.buttonSendKey);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textKeyInterval);
+            this.Controls.Add(this.textKey);
+            this.Controls.Add(this.textControlWindowName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ExtendHiGate);
+            this.Controls.Add(this.MixRun_1);
+            this.Controls.Add(this.click);
+            this.Controls.Add(this.NotifyIcon);
+            this.Controls.Add(this.stop);
+            this.Controls.Add(this.run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button run;
+        private System.Windows.Forms.Button stop;
+        private System.Windows.Forms.Button NotifyIcon;
+        private System.Windows.Forms.Button click;
+        private System.Windows.Forms.Button MixRun_1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button ExtendHiGate;
+        private System.Windows.Forms.TextBox textControlWindowName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonSendKey;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textKey;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textKeyInterval;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chBoxKeyPress;
+
+    }
+}
+
---
+++ WindowsMouseController/Form1.cs
@@ -0,0 +1,326 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
+
+namespace WindowsMouseController
+{
+    public partial class Form1 : Form
+    {
+        private System.Windows.Forms.NotifyIcon notifyIcon1; //建立icon的容器
+        System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer(); // 設定一個時鐘來模擬滑鼠
+        Action<string> action; //儲存目前當前執行的Thread
+        uint iRunMode = 0;
+
+        public delegate int HookProc(int nCode, IntPtr wParam, IntPtr lParam);
+        private static int m_HookHandle = 0;    // Hook handle
+        private HookProc m_KbdHookProc;            // 鍵盤掛鉤函式指標
+
+        //滑鼠控制常數
+        const int MOUSEEVENTF_ABSOLUTE = 0x8000;
+        const int MOUSEEVENTF_LEFTDOWN = 0x0002;
+        const int MOUSEEVENTF_LEFTUP = 0x0004;
+        const int MOUSEEVENTF_MIDDLEDOWN = 0x0020;
+        const int MOUSEEVENTF_MIDDLEUP = 0x0040;
+        const int MOUSEEVENTF_MOVE = 0x0001;
+        const int MOUSEEVENTF_RIGHTDOWN = 0x0008;
+        const int MOUSEEVENTF_RIGHTUP = 0x0010;
+        const int MOUSEEVENTF_WHEEL = 0x0800;
+        const int MOUSEEVENTF_XDOWN = 0x0080;
+        const int MOUSEEVENTF_XUP = 0x1000;
+        const int MOUSEEVENTF_HWHEEL = 0x01000;
+        const int WM_LBUTTONDOWN = 0x0201;
+        const int WM_LBUTTONUP = 0x0202;
+
+        //鍵盤控制常數
+        const int WH_KEYBOARD_LL = 13; //for global hook
+        const int KEYEVENTF_KEYUP = 0x0002;
+
+        /// <summary>
+        /// 透過windows user.dll，定義模擬滑鼠的各種API
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        static extern bool SetCursorPos(int X, int Y);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        //[DllImport("user32.dll", SetLastError = true)]
+        //static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+        //[DllImport("user32.dll", EntryPoint = "FindWindow", SetLastError = true)]
+        //public static extern IntPtr FindWindow(IntPtr ZeroOnly, string lpWindowName);
+
+        [DllImport("user32.dll")]
+        public static extern void keybd_event(byte bVk, byte bScan, uint fwFlag, IntPtr dwExtraInfo);
+
+
+        // 設置掛鉤.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int SetWindowsHookEx(int idHook, HookProc lpfn, IntPtr hInstance, int threadId);
+
+        // 將之前設置的掛鉤移除。記得在應用程式結束前呼叫此函式.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern bool UnhookWindowsHookEx(int idHook);
+
+         // 呼叫下一個掛鉤處理常式（若不這麼做，會令其他掛鉤處理常式失效）.
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern int CallNextHookEx(int idHook, int nCode, IntPtr wParam, IntPtr lParam);
+
+        private void LeftClick()
+        {
+            //int xpos = Control.MousePosition.X;
+            //int ypos = Control.MousePosition.Y;
+            int xpos = Cursor.Position.X;
+            int ypos = Cursor.Position.Y;
+            this.textBox1.AppendText("Click position X = " + xpos.ToString() + ",Y = " + ypos.ToString() + "\r\n");
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+        }
+        private void TimerEventMouseMove(Object myObject, EventArgs myEventArgs) {
+            int xpos = System.Windows.Forms.Cursor.Position.X;
+            int ypos = System.Windows.Forms.Cursor.Position.Y;
+            int xpos2 = System.Windows.Forms.Cursor.Position.X + Properties.Settings.Default.move_position_x;
+            int ypos2 = System.Windows.Forms.Cursor.Position.Y + Properties.Settings.Default.move_position_y;
+            SetCursorPos(xpos2, ypos2);
+            SetCursorPos(xpos, ypos);
+            return;
+        }
+
+        /// <summary>
+        /// 計時器觸發後當前座標滑鼠點擊
+        /// </summary>
+        /// <param name="myObject"></param>
+        /// <param name="myEventArgs"></param>
+        private void TimerEventMouseClick(Object myObject, EventArgs myEventArgs)
+        {
+            int xpos = System.Windows.Forms.Cursor.Position.X;
+            int ypos = System.Windows.Forms.Cursor.Position.Y;
+            //鍵盤控制
+            if (string.IsNullOrEmpty(textKey.Text) != true)
+            {
+                int key = Convert.ToInt32(textKey.Text);
+                if (key != 0)
+                {
+                    keybd_event((byte)key, 0, 0, IntPtr.Zero);
+                    keybd_event((byte)key, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+                }
+            }
+            Thread.Sleep(200);
+            //滑鼠控制
+            Debug.WriteLine("Move xpos: {0}, ypos: {1}", xpos, ypos);
+            Thread.Sleep(200);
+            SetCursorPos(xpos, ypos);
+            Thread.Sleep(200);
+            this.LeftClick();
+            return;
+        }
+
+        /// <summary>
+        /// 延長Higate時間的主程式
+        /// </summary>
+        /// <param name="myObject"></param>
+        /// <param name="myEventArgs"></param>
+        private void TimerEventSendMessageToHiGate(Object myObject, EventArgs myEventArgs)
+        {
+            IntPtr hwnd = IntPtr.Zero;
+            //hwnd = FindWindow(null,Properties.Settings.Default.higate_window);
+            if (hwnd == IntPtr.Zero)
+            {
+                MessageBox.Show("Couldn't find the " + Properties.Settings.Default.higate_window);
+            }
+            else
+            {
+                try
+                {
+                    SendMessage(hwnd, WM_LBUTTONDOWN, (IntPtr)0, IntPtr.Zero);
+                    SendMessage(hwnd, WM_LBUTTONUP, (IntPtr)0, IntPtr.Zero);
+                    //SendMessage(hwnd, BM_SETSTATE, 0, IntPtr.Zero);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        /// <summary>
+        /// 計時器觸發後移動到指定座標並連點
+        /// </summary>
+        /// <param name="myObject"></param>
+        /// <param name="myEventArgs"></param>
+        private void TimerEventMouseMoveAndClick1(Object myObject, EventArgs myEventArgs)
+        {
+            int xpos = System.Windows.Forms.Cursor.Position.X;
+            int ypos = System.Windows.Forms.Cursor.Position.Y;
+            int xpos2 = System.Windows.Forms.Cursor.Position.X + Properties.Settings.Default.move_position_x;
+            int ypos2 = System.Windows.Forms.Cursor.Position.Y + Properties.Settings.Default.move_position_y;
+            //儲存當前座標
+            Debug.WriteLine("Save: {0}, ypos: {1}", xpos, ypos);
+            this.textBox1.AppendText("Orignal xpos = " + xpos + ", ypos = " + ypos + "\n");
+            SetCursorPos(xpos, ypos);
+            this.LeftClick();
+            //移動到X座標move_position(Default=300)的地方
+            Debug.WriteLine("Move xpos: {0}, ypos: {1}", xpos2, ypos);
+            this.textBox1.AppendText("Move xpos = " + xpos2 + ", ypos = " + ypos + "\n");
+            SetCursorPos(xpos2, ypos2);
+            Debug.WriteLine("Move xpos2: {0}, ypos: {1}", xpos2, ypos);
+            this.LeftClick();
+            //返回原本座標
+            this.textBox1.AppendText("Return xpos = " + xpos + ", ypos = " + ypos + "\n");
+            Debug.WriteLine("Return xpos: {0}, ypos: {1}" , xpos, ypos);
+            SetCursorPos(xpos, ypos);
+            return;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+            //指定使用的容器
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            //建立NotifyIcon
+            this.notifyIcon1.Icon = new Icon(SystemIcons.Information, 40, 40);
+            this.notifyIcon1.Text = "Mouse Controller";
+            //點兩下Icon呼叫程式
+            this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_Click);
+        }
+
+        private void NotifyIcon_Click(object sender, EventArgs e)
+        {
+            if (this.notifyIcon1.Visible == false)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+            else
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.notifyIcon1.Visible = false;            
+            }
+        }
+
+        /// <summary>
+        /// 滑鼠移動, 設定一個計時器，時間到執行滑鼠移動
+        /// iRunMode = 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void run_Click(object sender, EventArgs e)
+        {
+            // timer.Interval = 60 sec = 60000 msec
+            int interval = Properties.Settings.Default.interval;
+            timer.Interval = (interval * 1000);
+            timer.Tick += TimerEventMouseMove;
+            timer.Start();
+            iRunMode = 1;
+            return;
+        }
+
+        /// <summary>
+        /// 滑鼠連點
+        /// iRunMode = 2
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void click_Click(object sender, EventArgs e)
+        {
+            // timer.Interval = 60 sec * 10 = 10 minute
+            int interval = Convert.ToInt32(textKeyInterval.Text);
+            if(interval == 0)
+                interval = 1;
+            timer.Interval = (interval * 1000);
+            timer.Tick += TimerEventMouseClick;
+            timer.Start();
+            iRunMode = 2;
+        }
+
+        /// <summary>
+        /// 左右連點
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void MixRun_1_Click(object sender, EventArgs e)
+        {
+            // timer.Interval = 60 sec * 10 = 10 minute
+            int interval = Properties.Settings.Default.interval;
+            timer.Interval = (interval * 1000);
+            timer.Tick += TimerEventMouseMoveAndClick1;
+            timer.Start();
+            iRunMode = 3;
+        }
+
+        private void ExtendHiGate_Click(object sender, EventArgs e)
+        {
+            // timer.Interval = 60 sec * 10 = 10 minute
+            int interval = Properties.Settings.Default.interval;
+            timer.Interval = (interval * 1000);
+            timer.Tick += TimerEventSendMessageToHiGate;
+            timer.Start();
+            iRunMode = 4;
+        }
+
+        private bool _start;
+        private void buttonSendKey_Click(object sender, EventArgs e)
+        {
+            _start = true;
+            while (_start)
+            {
+                int key = Convert.ToInt32(textKey.Text);
+                int interval = Convert.ToInt32(textKeyInterval.Text);
+                if(key == 0 || interval == 0)
+                    MessageBox.Show("鍵盤參數錯誤");
+                const int KEYEVENTF_KEYUP = 0x0002;
+                keybd_event((byte)key, 0, 0, IntPtr.Zero);
+                keybd_event((byte)key, 0, KEYEVENTF_KEYUP, IntPtr.Zero);
+                Thread.Sleep(interval * 1000);
+                Application.DoEvents(); 
+            }
+        }
+
+        /// <summary>
+        /// 觸發計時器中止且釋放timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void stop_Click(object sender, EventArgs e)
+        {
+            //For Mouse control
+            if (timer.Enabled)
+                timer.Stop();
+
+            //To terminate runnning thread
+            switch (iRunMode)
+            {
+                case 1:
+                    timer.Tick -= TimerEventMouseMove;
+                    break;
+                case 2:
+                    timer.Tick -= TimerEventMouseClick;
+                    break;
+                case 3:
+                    timer.Tick -= TimerEventMouseMoveAndClick1;
+                    break;
+                case 4:
+                    timer.Tick -= TimerEventSendMessageToHiGate;
+                    break;
+            }
+            //For Keyboard control
+            if (_start)
+                _start = false;
+            return;
+        }
+    }
+}
